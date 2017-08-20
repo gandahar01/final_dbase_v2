f5f403f70e8f6288cb8405dfaae9d3c2a89edc49
Raghu Betina <raghu@firstdraft.com>
2017-08-20 23:43:00 +0000

    Generate foods resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Food resource:
+  # CREATE
+  get "/foods/new", :controller => "foods", :action => "new"
+  post "/create_food", :controller => "foods", :action => "create"
+
+  # READ
+  get "/foods", :controller => "foods", :action => "index"
+  get "/foods/:id", :controller => "foods", :action => "show"
+
+  # UPDATE
+  get "/foods/:id/edit", :controller => "foods", :action => "edit"
+  post "/update_food/:id", :controller => "foods", :action => "update"
+
+  # DELETE
+  get "/delete_food/:id", :controller => "foods", :action => "destroy"
+  #------------------------------
+
   # Routes for the Weekly_workout resource:
   # CREATE
   get "/weekly_workouts/new", :controller => "weekly_workouts", :action => "new"
