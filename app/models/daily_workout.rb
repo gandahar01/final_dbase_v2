ec32b5260d73b38fdf0fe0a36b1399787cb628cc
Raghu Betina <raghu@firstdraft.com>
2017-08-20 23:43:22 +0000

    Direct association between weekly_workouts and daily_workouts

--- app/models/daily_workout.rb
+++ app/models/daily_workout.rb
@@ -1,6 +1,9 @@
 class DailyWorkout < ApplicationRecord
   # Direct associations
 
+  belongs_to :weeklyworkout,
+             :class_name => "WeeklyWorkout"
+
   belongs_to :user
 
   # Indirect associations
--- app/models/weekly_workout.rb
+++ app/models/weekly_workout.rb
@@ -1,6 +1,10 @@
 class WeeklyWorkout < ApplicationRecord
   # Direct associations
 
+  has_many   :daily_workouts,
+             :foreign_key => "weeklyworkout_id",
+             :dependent => :destroy
+
   belongs_to :user
 
   # Indirect associations
