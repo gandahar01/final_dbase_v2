0b3e86d255496f59fd72e58bbcfa19d3a24833d1
Raghu Betina <raghu@firstdraft.com>
2017-08-20 23:43:22 +0000

    Direct association between foods_catagories and foods_nutritions

--- app/models/foods_catagory.rb
+++ app/models/foods_catagory.rb
@@ -1,6 +1,10 @@
 class FoodsCatagory < ApplicationRecord
   # Direct associations
 
+  has_many   :foods_nutritions,
+             :foreign_key => "foodscatagory_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/foods_nutrition.rb
+++ app/models/foods_nutrition.rb
@@ -1,6 +1,9 @@
 class FoodsNutrition < ApplicationRecord
   # Direct associations
 
+  belongs_to :foodscatagory,
+             :class_name => "FoodsCatagory"
+
   belongs_to :foods,
              :class_name => "Food"
 
