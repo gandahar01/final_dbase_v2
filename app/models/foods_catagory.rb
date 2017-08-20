2c5f140d71a9996645d3d6d08174ffa76b62b6b1
Raghu Betina <raghu@firstdraft.com>
2017-08-20 23:43:22 +0000

    Direct association between foods_catagories and user_cheat_allowances

--- app/models/foods_catagory.rb
+++ app/models/foods_catagory.rb
@@ -1,6 +1,10 @@
 class FoodsCatagory < ApplicationRecord
   # Direct associations
 
+  has_many   :user_cheat_allowances,
+             :foreign_key => "foodscatagory_id",
+             :dependent => :destroy
+
   has_many   :foods_nutritions,
              :foreign_key => "foodscatagory_id",
              :dependent => :destroy
--- app/models/user_cheat_allowance.rb
+++ app/models/user_cheat_allowance.rb
@@ -1,6 +1,9 @@
 class UserCheatAllowance < ApplicationRecord
   # Direct associations
 
+  belongs_to :foodscatagory,
+             :class_name => "FoodsCatagory"
+
   belongs_to :user
 
   # Indirect associations
