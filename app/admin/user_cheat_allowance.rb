3ab7c5e3e1943308bb40b81c8dd1278afb3dfaae
Raghu Betina <raghu@firstdraft.com>
2017-08-20 23:44:15 +0000

    Whitelist columns in UserCheatAllowance for ActiveAdmin

--- app/admin/user_cheat_allowance.rb
+++ app/admin/user_cheat_allowance.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register UserCheatAllowance do
 
+ permit_params :cheat_calories, :cheat_carbs, :cheat_fat, :cheat_sugar, :week_date, :user_id, :foodscatagory_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
